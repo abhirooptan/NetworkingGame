638ad4d30c9c6da7bcc16d1c8bc5f91a444721fa
Abhiroop Tandon <abhirooptan@live.com>
2016-07-07 10:29:17 +0100

    Initial Commit

---
+++ Assets/SwordDamage.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordDamage : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Sword Initialized");
+    }
+
+    void OnCollisioEnter(Collision collision)
+    {
+        var hit = collision.gameObject;
+        Debug.Log("Damage: " + hit.name);
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+
+        //Destroy(gameObject);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        var hit = other.gameObject;
+        Debug.Log("Trigger Damage: " + hit.name);
+        var health = hit.GetComponent<Health>();
+        if (health != null)
+        {
+            health.TakeDamage(10);
+        }
+    }
+}
