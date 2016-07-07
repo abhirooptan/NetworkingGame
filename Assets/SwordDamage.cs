32f64e6b40c1cc7fe5437d9f995661acc7b71277
Abhiroop Tandon <abhirooptan@live.com>
2016-07-07 17:04:08 +0100

    BloodShed Included

--- Assets/SwordDamage.cs
+++ Assets/SwordDamage.cs
@@ -1,34 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class SwordDamage : MonoBehaviour
+public class SwordDamage : NetworkBehaviour
 {
+    public GameObject blood;
     void Start()
     {
         Debug.Log("Sword Initialized");
     }
 
-    void OnCollisioEnter(Collision collision)
-    {
-        var hit = collision.gameObject;
-        Debug.Log("Damage: " + hit.name);
-        var health = hit.GetComponent<Health>();
-        if (health != null)
-        {
-            health.TakeDamage(10);
-        }
-
-        //Destroy(gameObject);
-    }
-
     public void OnTriggerEnter(Collider other)
     {
         var hit = other.gameObject;
-        Debug.Log("Trigger Damage: " + hit.name);
-        var health = hit.GetComponent<Health>();
-        if (health != null)
+        if (hit.gameObject == isLocalPlayer)
         {
-            health.TakeDamage(10);
+            Debug.Log("Dont hit me i am the local one");
+            // do nothing
+        }
+        else
+        {
+            Debug.Log("Trigger Damage: " + hit.name);
+            var health = hit.GetComponent<Health>();
+            if (health != null)
+            {
+                var bloodshed = Instantiate(blood, transform.position, transform.rotation);
+                Destroy(bloodshed, 1.0f);
+                health.TakeDamage(10);
+            }
         }
     }
 }
